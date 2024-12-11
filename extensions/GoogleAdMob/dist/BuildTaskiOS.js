c7392b44325594a38cdae12784200e2252ed8363
hoanghdtv <hoanghdtv154@gmail.com>
2024-12-11 17:16:18 +0700

    .

--- extensions/GoogleAdMob/dist/BuildTaskiOS.js
+++ extensions/GoogleAdMob/dist/BuildTaskiOS.js
@@ -1,7 +1,34 @@
 "use strict";
+/*
+ Copyright (c) 2023-2024 Xiamen Yaji Software Co., Ltd.
+
+ https://www.cocos.com/
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -11,13 +38,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildTaskiOS = exports.BuildTaskiOS = void 0;
 const builder_1 = require("./builder");
--- extensions/GoogleAdMob/dist/util.js
+++ extensions/GoogleAdMob/dist/util.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -11,15 +15,28 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteDestToSrcBehindKey = exports.insertDestToSrcBehindKey = exports.appendDestContentToSrcFileIfNo = exports.deleteDestContentInSrcFile = void 0;
+exports.deleteDestContentInSrcFile = deleteDestContentInSrcFile;
+exports.appendDestContentToSrcFileIfNo = appendDestContentToSrcFileIfNo;
+exports.insertDestToSrcBehindKey = insertDestToSrcBehindKey;
+exports.deleteDestToSrcBehindKey = deleteDestToSrcBehindKey;
 /*
  Copyright (c) 2023-2024 Xiamen Yaji Software Co., Ltd.
 
@@ -54,7 +71,6 @@ function deleteDestContentInSrcFile(srcPath, destPath) {
         fs.writeFileSync(srcPath, userGradle);
     }
 }
-exports.deleteDestContentInSrcFile = deleteDestContentInSrcFile;
 function appendDestContentToSrcFileIfNo(srcPath, destPath) {
     let userGradle = fs.readFileSync(srcPath, { encoding: 'binary' });
     let template = fs.readFileSync(destPath, { encoding: 'binary' });
@@ -63,7 +79,6 @@ function appendDestContentToSrcFileIfNo(srcPath, destPath) {
         fs.writeFileSync(srcPath, userGradle + template);
     }
 }
-exports.appendDestContentToSrcFileIfNo = appendDestContentToSrcFileIfNo;
 function insertDestToSrcBehindKey(srcPath, destPath, keyPath) {
     const srcContent = fs.readFileSync(srcPath, { encoding: 'binary' });
     const keyCodeContent = fs.readFileSync(keyPath, { encoding: 'binary' });
@@ -82,7 +97,6 @@ function insertDestToSrcBehindKey(srcPath, destPath, keyPath) {
         }
     }
 }
-exports.insertDestToSrcBehindKey = insertDestToSrcBehindKey;
 /**
  * @en
  * delete content from destPath in the content of srcPath.
@@ -98,4 +112,3 @@ function deleteDestToSrcBehindKey(srcPath, destPath) {
         fs.writeFileSync(srcPath, final);
     }
 }
-exports.deleteDestToSrcBehindKey = deleteDestToSrcBehindKey;
